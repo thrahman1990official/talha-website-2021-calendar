07746e035d232bbe8ed46316a6dec57d0fa7ad79
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2021-10-12 17:58:46 -0400

    Update calender.js

--- calender.js
+++ calender.js
@@ -1,1 +1,9 @@
-
+function getDatesBetween(date1, date2)
+{
+let range1 = new Date(date1);
+let range2 = new Date(date2);
+console.log(range1 + " " + range2);
+}
+  
+let content = getDatesBetween("2020/01/01", "2021/01/01");
+document.getElementById("calender").innerHTML = content;
