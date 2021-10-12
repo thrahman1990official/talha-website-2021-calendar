e46a63d8e8893982a9c92ec36918e1fd6048c483
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2021-10-12 17:22:26 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>This is Talha Rahman's Skills-Page</title>
+    <style>
+      body  {background-color: black;}
+      table {border: 1px solid red;}
+      th,td {border: 1px solid red;}
+      h1    {color: Yellow;  font-family: impact;}
+      th    {color: orange;  font-family: impact;}
+      td    {color: white;  font-family: times; font-size: 14px; text-align: justify;}
+    </style>
+  </head>
+  <body>
+    <h1><center>Talha Rahman's Skills Page</center></h1>
+    <center>
+    <table>
+
+      
+      
+    </table>
+    </center>
+    </body>
+</html>
