670de3cc442d5990565656dbf02c05b1ca322a6c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2021-10-13 04:32:29 -0400

    Update calender.js

--- calender.js
+++ calender.js
@@ -12,49 +12,37 @@ function getDatesBetween(date1, date2)
   date1 = settingDate(date1, 31);
   date2 = settingDate(date2, 31);
   let temp;
-  let dates = [];
+  let dates=[];
   
-    while(date1 <= date2)
+    while(date1<=date2)
       {
-      if(date1.getDate()!=31)
+      if(date1.getDate() != 31)
         {
-        temp=settingDate(date1,0); 
-          if(temp>=range1 && temp<=range2) dates.push(temp);
-        date1=settingDate(date1, 31);
+        temp = settingDate(date1, 0); 
+          if (temp >= range1 && temp <= range2) dates.push(temp);
+        date1 = settingDate(date1, 31);
         }
       else
         {
-        temp = new Date(date1);
-           if (temp >= range1 && temp <= range2) dates.push(temp);
+        temp=new Date(date1);
+           if(temp>=range1 && temp<=range2) dates.push(temp);
         date1.setMonth(date1.getMonth() + 1);
+        }
       }
-}
-console.log(dates);
-let content = "";
-let weekDays = 
- [{ shortDay: "Mon", fullDay: "Monday" },
- { shortDay: "Tue", fullDay: "Tuesday" },
- { shortDay: "Wed", fullday: "Wednesday" },
- { shortDay: "Thu", fullDay: "Thursday" },
- { shortDay: "Fri", fullDay: "Friday" },
- { shortDay: "Sat", fullDay: "Saturday" },
- { shortDay: "Sun", fullDay: "Sunday" }];
+    console.log(dates);
+    let content="";
+    let weekDays=[{ shortDay: "Mon", fullDay: "Monday" },{ shortDay: "Tue", fullDay: "Tuesday" },{ shortDay: "Wed", fullday: "Wednesday" },{ shortDay: "Thu", fullDay: "Thursday" },{ shortDay: "Fri", fullDay: "Friday" },{ shortDay: "Sat", fullDay: "Saturday" },{ shortDay: "Sun", fullDay: "Sunday" }];
  
  let LastDate, firstDate;
- for(let i = 0; i < dates.length; i++)
+ for(let i=0; i<dates.length; i++)
  {
-  LastDate = dates[i];
-  firstDate = new Date(dates[i].getFullYear(), dates[i].getMonth(), 1);
-  content += "<div id='calenderTable_" + (i + 1) + "'>";
-  content += 
-    "<h2>" + 
-    firstDate.toString().split(" ")[0] + 
-    "-" + 
-    firstDate.getFullYear() + 
-    "</h2>";
-  content += "</div>";
+  LastDate=dates[i];
+  firstDate= new Date(dates[i].getFullYear(), dates[i].getMonth(), 1);
+  content+="<div id='calenderTable_"+(i+1)+"'>";
+  content+="<h2>"+firstDate.toString().split(" ")[0]+"-"+firstDate.getFullYear()+"</h2>";
+  content+="</div>";
  }
  return content;
-}  
+}
 let content = getDatesBetween("2020/01/01", "2021/01/01");
 document.getElementById("calender").innerHTML = content;
